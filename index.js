7321519b9c084ad0120c56302151477770be854f
Sarthak431 <136489174+Sarthak431@users.noreply.github.com>
2023-07-29 19:52:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+let A = [1, 2, 3, "b"];
+
+for (let i = 0; i < A.length; i++) {
+  document.getElementById("A").innerHTML += A[i] + " ";
+}
+
+let B = new Array(1, 5, 2);
+
+document.getElementById("A").innerHTML += "<br>";
+
+for (let i = 0; i < B.length; i++) {
+  document.getElementById("B").innerText += B[i];
+}
+
+const C = "Hello World";
+
+const D = new String("Sarthak Singh");
+
+document.getElementById('B').innerHTML +="<br>"+ C; 
+document.getElementById('B').innerHTML +="<br>"+ D; 
+
+
+
+
+// Arithmetic Operators: These are used for basic arithmetic calculations.
+
+// Addition (+)
+// Subtraction (-)
+// Multiplication (*)
+// Division (/)
+// Modulus (%)
+// Increment (++)
+// Decrement (--)
+// Comparison Operators: These are used to compare two values.
+
+// Equal to (==)
+// Not equal to (!=)
+// Strict equal to (===)
+// Strict not equal to (!==)
+// Greater than (>)
+// Less than (<)
+// Greater than or equal to (>=)
+// Less than or equal to (<=)
+// Logical Operators: These are used to combine or negate conditions.
+
+// Logical AND (&&)
+// Logical OR (||)
+// Logical NOT (!)
+// Assignment Operators: These are used to assign values to variables.
+
+// Assignment (=)
+// Addition assignment (+=)
+// Subtraction assignment (-=)
+// Multiplication assignment (*=)
+// Division assignment (/=)
+// Modulus assignment (%=)
+// Bitwise Operators: These are used to perform bitwise operations.
+
+// Bitwise AND (&)
+// Bitwise OR (|)
+// Bitwise XOR (^)
+// Bitwise NOT (~)
+// Left shift (<<)
+// Right shift (>>)
+// Zero-fill right shift (>>>)
+
+
